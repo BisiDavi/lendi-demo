ae3281aa72466bebc52f015d88735dc329faed02
BisiDavi <oludavidconnect@gmail.com>
2021-08-02 14:45:57 +0100

    main

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,20 +14,40 @@ export default function Footer() {
   return (
     <footer className={s.footer}>
       <div className="container p-4 m-auto">
-        <h4 className="font-normal text-white text-3xl my-3">Still looking?</h4>
-        <div className="footer-links flex m-auto justify-center">
-          {footerLinks.map((footerLink, index) => (
-            <div key={index} className={`row-${index} w-1/4`}>
-              {displayLink(footerLink.main, "font-medium text-white text-2xl")}
-              <hr />
-              <div className="sub flex align-items flex-col">
+        <div className="content px-28">
+          <h4 className="font-normal text-white text-3xl my-3">
+            Still looking?
+          </h4>
+          <div className="footer-links flex m-auto justify-center">
+            {footerLinks.siteLink.map((footerLink, index) => (
+              <div key={index} className={`row-${index} w-1/4`}>
                 {displayLink(
-                  footerLink.sub,
-                  "font-medium text-white text-lg my-2"
+                  footerLink.main,
+                  "font-medium text-white text-xl my-2"
                 )}
+                <hr className="my-3" />
+                <div className="sub flex align-items flex-col">
+                  {displayLink(
+                    footerLink.sub,
+                    "font-medium text-white text-lg my-2"
+                  )}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          <div className="pageLinks flex m-auto item-center justify-center my-12">
+            {footerLinks.sitePages.map((siteLink, index) => (
+              <li key={index}>
+                <Link href={siteLink.link} passHref>
+                  <a className="text-center text-white text-xl mx-3">
+                    {" "}
+                    {siteLink.name}
+                  </a>
+                </Link>{" "}
+                <span className="text-white mx-2 font-medium">|</span>
+              </li>
+            ))}
+          </div>
         </div>
       </div>
     </footer>
