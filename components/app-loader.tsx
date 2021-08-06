663c73d543416b5eb249f332006e1ab84bf9d711
BisiDavi <oludavidconnect@gmail.com>
2021-08-06 17:48:40 +0100

    css fix

--- components/app-loader.tsx
+++ components/app-loader.tsx
@@ -4,7 +4,7 @@ function Loader() {
   return (
     <Contentloader
       speed={2}
-      className="rounded-lg rounded-lg lg:items-center flex m-2 lg:flex-row flex-col lg:flex-row"
+      className="rounded-lg character card rounded-lg lg:items-center bg-gray-100 flex m-2 lg:flex-row flex-col lg:flex-row"
       style={{ width: "90%" }}
       backgroundColor={"lightgray"}
       foregroundColor={"#999"}
@@ -17,7 +17,7 @@ function Loader() {
 export default function AppLoader() {
   const contentsExpected = Array(20);
   return (
-    <div className="apploader lg:my-5 grid grid-cols-2 lg:grid-cols-4 md:grid-cols-3 lg:px-5">
+    <div className="apploader lg:my-5 grid grid-cols-2 md:grid-cols-3  lg:grid-cols-3  xl:grid-cols-4 2xl:grid-cols-5 lg:px-5">
       {contentsExpected.fill(20).map((_, index) => (
         <Loader key={index} />
       ))}
