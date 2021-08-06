ad39a500ecf00bf4416996e145e1250b169cb0af
BisiDavi <oludavidconnect@gmail.com>
2021-08-06 17:53:52 +0100

    endpoint url added

--- client/client.ts
+++ client/client.ts
@@ -1,6 +1,6 @@
 import ApolloClient from "apollo-boost";
 
-const endpoint = process.env.NEXT_PUBLIC_ENDPOINT;
+const endpoint = "https://rickandmortyapi.com/graphql";
 
 export const client: any = new ApolloClient({
   uri: endpoint,
